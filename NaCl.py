cac35d471520e200f84ec19320988a6ccfe633ad
Annika Hammervoll <annika_limfe@hotmail.com>
2017-10-16 15:18:49 +0200

    NaCl.py: Cleaning up print and error output (with line and column numbers)

--- NaCl.py
+++ NaCl.py
@@ -92,7 +92,7 @@ class Element(object):
 		self.members = {}
 
 	def process(self):
-		sys.exit("Error (" + self.name + "): Subclass of class Element has not implemented the process method")
+		sys.exit("Internal error line " + get_line_and_column(self.ctx) + ": Subclass of class Element has not implemented the process method")
 
 	# ---------- Methods related to dictionary self.members ----------
 
@@ -131,7 +131,7 @@ class Element(object):
 				return
 
 		if level_key not in dictionary:
-			sys.exit("Error (" + self.name + "): Trying to add to a member (" + level_key + ") that doesn't exist")
+			sys.exit("Error line " + get_line_and_column(value) + ": Trying to add to a member (" + level_key + ") that doesn't exist")
 
 		for key in dictionary:
 			if key == level_key:
@@ -143,7 +143,7 @@ class Element(object):
 			key = pair.key().getText()
 
 			if dictionary.get(key) is not None:
-				sys.exit("Error(" + self.name + "): Member " + key + " has already been set")
+				sys.exit("Error line " + get_line_and_column(pair.key()) + ": Member " + key + " has already been set")
 
 			# End of recursion:
 			if pair.value().obj() is None:
@@ -178,7 +178,7 @@ class Untyped(Element):
 				# Check if this key has already been set in this Untyped element
 				# In that case: Error: Value already set
 				if self.get_dictionary_val(self.members, list(assignment_name_parts)) is not None:
-					sys.exit("Error (" + self.name + "): Member " + element.name + " has already been set")
+					sys.exit("Error line " + get_line_and_column(element.ctx) + ": Member " + element.name + " has already been set")
 				else:
 					# Add to members dictionary
 					self.add_dictionary_val(self.members, assignment_name_parts, element.ctx.value())
@@ -195,7 +195,7 @@ class Untyped(Element):
 				
 				element = elements.get(element_name)
 				if element is None:
-					sys.exit("Error (" + self.name + "): No element with the name " + element_name + " exists")
+					sys.exit("Error line " + get_line_and_column(self.ctx) + ": No element with the name " + element_name + " exists")
 			else:
 				if self.ctx.value().obj() is not None:
 					# Then this element is an obj and other assignments can add to this element
@@ -204,8 +204,6 @@ class Untyped(Element):
 					
 					self.process_obj(self.members, self.ctx.value().obj())
 					self.process_assignments();
-
-					print self.name, ": Members after checking assignments related to this element:", str(self.members)
 
 			self.res = self.members
 
@@ -256,15 +254,15 @@ class Iface(Typed):
 			# More than one function pushed onto chain
 			for list_value in value_ctx.list_t().value_list().value():
 				if list_value.value_name() is None:
-					sys.exit("Error (" + self.name + "): This is not supported: " + pair_ctx.getText())
+					sys.exit("Error line " + get_line_and_column(list_value) + ": This is not supported: " + pair_ctx.getText())
 				functions.append(list_value.value_name().getText())
 		elif value_ctx.value_name() is not None:
 			# Only one function pushed onto chain
-			functions = [value_ctx.value_name().getText()]
+			functions = [ value_ctx.value_name().getText() ]
 		else:
-			sys.exit("Error (" + self.name + "): This is not supported: " + pair_ctx.getText())
+			sys.exit("Error line " + get_line_and_column(value_ctx) + ": This is not supported: " + pair_ctx.getText())
 		
-		self.add_push(chain, functions)
+		self.add_push(chain, functions, value_ctx)
 
 	def process_ctx(self):
 		# Handle the Iface's value ctx object: Fill self.members dictionary
@@ -277,10 +275,10 @@ class Iface(Typed):
 				pair_value 	= pair.value()
 
 				if key not in predefined_iface_keys:
-					sys.exit("Error (" + self.name + "): Invalid member (" + orig_key + ") inside Iface object")
+					sys.exit("Error line " + get_line_and_column(pair.key()) + ": Invalid Iface member " + orig_key)
 
 				if self.members.get(key) is not None:
-					sys.exit("Error (" + self.name + "): Member " + key + " has already been set")
+					sys.exit("Error line " + get_line_and_column(pair.key()) + ": Iface member " + key + " has already been set")
 
 				if key in chains:
 					self.process_push(key, pair);
@@ -296,9 +294,9 @@ class Iface(Typed):
 			if config in predefined_config_types:
 				self.members[IFACE_KEY_CONFIG] = value
 			else:
-				sys.exit("Error (" + self.name + "): Invalid Iface value " + value.value_name().getText())
+				sys.exit("Error line " + get_line_and_column(value) + ": Invalid Iface value " + value.value_name().getText())
 		else:
-			sys.exit("Error (" + self.name + "): An Iface has to contain key value pairs, or be set to a configuration type (" + \
+			sys.exit("Error line " + get_line_and_column(value) + ": An Iface has to contain key value pairs, or be set to a configuration type (" + \
 				", ".join(predefined_config_types) + ")")
 
 	def process_assignments(self):
@@ -314,13 +312,13 @@ class Iface(Typed):
 				iface_member = orig_iface_member.lower()
 
 				if len(name_parts) != 2:
-					sys.exit("Error (" + element.name + "): Invalid Iface member " + key)
+					sys.exit("Error line " + get_line_and_column(element.ctx) + ": Invalid Iface member " + key)
 
 				if iface_member not in predefined_iface_keys:
-					sys.exit("Error (" + element.name + "): Invalid member (" + orig_iface_member + ") of Iface object")
+					sys.exit("Error line " + get_line_and_column(element.ctx) + ": Invalid Iface member " + orig_iface_member)
 				
 				if self.members.get(iface_member) is not None:
-					sys.exit("Error (" + self.name + "): Member " + iface_member + " has already been set")
+					sys.exit("Error line " + get_line_and_column(element.ctx) + ": Member " + iface_member + " has already been set")
 
 				found_element_value = element.ctx.value()
 				if iface_member in chains:
@@ -332,15 +330,15 @@ class Iface(Typed):
 						# More than one function pushed onto chain
 						for list_value in found_element_value.list_t().value_list().value():
 							if list_value.value_name() is None:
-								sys.exit("Error (" + element.name + "): This is not supported: " + element.ctx.getText())
+								sys.exit("Error line " + get_line_and_column(list_value) + ": This is not supported: " + element.ctx.getText())
 							functions.append(list_value.value_name().getText())
 					elif found_element_value.value_name() is not None:
 						# Only one function pushed onto chain
-						functions = [found_element_value.value_name().getText()]
+						functions = [ found_element_value.value_name().getText() ]
 					else:
-						sys.exit("Error (" + element.name + "): This is not supported: " + element.ctx.getText())
+						sys.exit("Error line " + get_line_and_column(found_element_value) + ": This is not supported: " + element.ctx.getText())
 
-					self.add_push(iface_member, functions)
+					self.add_push(iface_member, functions, found_element_value)
 					# pushes_to_add[iface_member] = functions
 					# else:
 					#	sys.exit("Error (" + element.name + "): Iface chain " + iface_member + " has already been set")
@@ -349,7 +347,7 @@ class Iface(Typed):
 						if self.members.get(iface_member) is None:
 							self.members[iface_member] = found_element_value
 						else:
-							sys.exit("Error (" + element.name + "): Iface member " + iface_member + " has already been set")
+							sys.exit("Error line " + get_line_and_column(element.ctx) + ": Iface member " + iface_member + " has already been set")
 					else:
 						masq_val = resolve_value(LANGUAGE, found_element_value)
 						# TODO:
@@ -360,18 +358,18 @@ class Iface(Typed):
 
 	def validate_members(self):
 		if self.members.get(IFACE_KEY_INDEX) is None:
-			sys.exit("Error (" + self.name + "): An index needs to be specified for all Ifaces")
+			sys.exit("Error line " + get_line_and_column(self.ctx.value()) + ": An index needs to be specified for all Ifaces")
 
 		config = self.members.get(IFACE_KEY_CONFIG)
 		if config is not None and (config.value_name() is None or config.value_name().getText().lower() not in predefined_config_types):
-			sys.exit("Error (" + self.name + "): Invalid config value " + config.getText())
+			sys.exit("Error line " + get_line_and_column(config) + ": Invalid config value " + config.getText())
 
 		if (config is None or config.value_name().getText().lower() != DHCP_CONFIG) and \
 			(self.members.get(IFACE_KEY_ADDRESS) is None or \
 				self.members.get(IFACE_KEY_NETMASK) is None or \
 				self.members.get(IFACE_KEY_GATEWAY) is None):
-			sys.exit("Error (" + self.name + "): The members address, netmask and gateway must be set for every Iface if the Iface " + \
-				"configuration hasn't been set to " + DHCP_CONFIG)
+			sys.exit("Error line " + get_line_and_column(self.ctx.value()) + ": The members address, netmask and gateway must be set for " + \
+				"every Iface if the Iface configuration hasn't been set to " + DHCP_CONFIG)
 
 	def process_members(self):
 		# Masquerade
@@ -404,15 +402,15 @@ class Iface(Typed):
 						vlan_name = v.value_name().getText()
 						vlan_element = elements.get(vlan_name)
 						if vlan_element is None or not hasattr(vlan_element, 'type_t') or vlan_element.type_t.lower() != TYPE_VLAN:
-							sys.exit("Error (" + self.name + "): Undefined Vlan element " + vlan_name)
+							sys.exit("Error line " + get_line_and_column(v) + ": Undefined Vlan " + vlan_name)
 					elif v.obj() is not None:
 						vlan_element = Vlan(0, "", v, BASE_TYPE_TYPED_INIT, TYPE_VLAN)
 					else:
-						sys.exit("Error (" + self.name + "): A Vlan list must either contain names of vlans or defined Vlan objects")
+						sys.exit("Error line " + get_line_and_column(v) + ": A Vlan list must either contain Vlan objects (key value pairs) or names of Vlans")
 
 					vlans.append(vlan_element)
 			else:
-				sys.exit("Error (" + self.name + "): An Iface's Vlan needs to be a list of Vlans")
+				sys.exit("Error line " + get_line_and_column(vlan_ctx) + ": An Iface's vlan needs to be a list of Vlans")
 
 		# Loop through self.members and resolve the values
 		# Note: If a member's value is None, the new value will be "" here:
@@ -439,7 +437,7 @@ class Iface(Typed):
 			# Process and add vlans found
 			self.add_iface_vlans(vlans)
 
-	def add_push(self, chain, functions):
+	def add_push(self, chain, functions, ctx):
 		# chain: string with name of chain
 		# functions: list containing strings, where each string corresponds to the name of a NaCl function
 		
@@ -448,7 +446,7 @@ class Iface(Typed):
 		for i, function in enumerate(functions):
 			element = elements.get(function)
 			if element is None or element.base_type != BASE_TYPE_FUNCTION:
-				sys.exit("Error: No function (Filter, Nat) with the name " + function + " exists")
+				sys.exit("Error line " + get_line_and_column(ctx) + ": No function with the name " + function + " exists")
 			function_names.append({TEMPLATE_KEY_FUNCTION_NAME: function, TEMPLATE_KEY_COMMA: (i < (num_functions - 1))})
 
 		pushes.append({
@@ -512,8 +510,6 @@ class Iface(Typed):
 			self.process_members()
 			self.add_iface()
 
-			print "Processed iface with name " + self.name
-
 			self.res = self.members
 			# Or:
 			# self.res = resolve_value(LANGUAGE, ...)
@@ -537,7 +533,6 @@ class Vlan(Typed):
 	def process_ctx(self):
 		# Handle the Vlan's value ctx object: Fill self.members dictionary
 
-		# value = self.ctx.value()
 		value = self.ctx.value() if hasattr(self.ctx, 'value') else self.ctx
 
 		if value.obj() is not None:
@@ -547,14 +542,14 @@ class Vlan(Typed):
 				pair_value 	= pair.value()
 
 				if key not in predefined_vlan_keys:
-					sys.exit("Error (" + self.name + "): Invalid member (" + orig_key + ") inside Vlan object")
+					sys.exit("Error line " + get_line_and_column(pair.key()) + ": Invalid Vlan member " + orig_key)
 
 				if self.members.get(key) is not None:
-					sys.exit("Error (" + self.name + "): Member " + key + " has already been set")
+					sys.exit("Error line " + get_line_and_column(pair.key()) + ": Vlan member " + key + " has already been set")
 
 				self.members[key] = pair_value
 		else:
-			sys.exit("Error (" + self.name + "): A Vlan has to contain key value pairs")
+			sys.exit("Error line " + get_line_and_column(value) + ": A Vlan has to contain key value pairs")
 
 	def process_assignments(self):
 		# Loop through elements that are assignments
@@ -569,21 +564,21 @@ class Vlan(Typed):
 				vlan_member = orig_vlan_member.lower()
 
 				if len(name_parts) != 2:
-					sys.exit("Error (" + element.name + "): Invalid Vlan member " + key)
+					sys.exit("Error line " + get_line_and_column(element.ctx) + ": Invalid Vlan member " + key)
 
 				if vlan_member not in predefined_vlan_keys:
-					sys.exit("Error (" + element.name + "): Invalid member (" + orig_vlan_member + ") of Vlan object")
+					sys.exit("Error line " + get_line_and_column(element.ctx) + ": Invalid Vlan member " + orig_vlan_member)
 
 				found_element_value = element.ctx.value()
 				if self.members.get(vlan_member) is None:
 					self.members[vlan_member] = found_element_value
 				else:
-					sys.exit("Error (" + self.name + "): Member " + vlan_member + " has already been set")
+					sys.exit("Error line " + get_line_and_column(element.ctx) + ": Member " + vlan_member + " has already been set")
 
 	def validate_members(self):
 		if self.members.get(VLAN_KEY_INDEX) is None or self.members.get(VLAN_KEY_ADDRESS) is None or \
 			self.members.get(VLAN_KEY_NETMASK) is None:
-			sys.exit("Error (" + self.name + "): The members index, address and netmask must be set for every Vlan")
+			sys.exit("Error line " + get_line_and_column(self.ctx) + ": The members index, address and netmask must be set for every Vlan")
 
 	def process_members(self):
 		# Transpile values
