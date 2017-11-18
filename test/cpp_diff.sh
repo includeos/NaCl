755947e594e123053965feab903d70d18a9f53ef
Annika Hammervoll <annika_limfe@hotmail.com>
2017-11-18 10:53:55 +0100

    test/cpp_diff.sh Testing cpp diff of all cpp files in goldenfiles using a for-loop

--- test/cpp_diff.sh
+++ test/cpp_diff.sh
@@ -1,159 +1,35 @@
 #!/bin/bash
 
 DIFFERENCE=""
+CPP_OUTPUT="nacl_content.cpp"
+GOLDENFILES="goldenfiles"
 
-# TODO: For loop
+declare -a examples=(
+	"assignments"
+	"cidr"
+	"config_options"
+	"conntrack"
+	"functions"
+	"gateway_with_forward_chain"
+	"gateway_with_send_time_exceeded"
+	"iface"
+	"log"
+	"nacl"
+	"nacl_one_liner"
+	"nat_and_gateway"
+	"vlan"
+)
 
-# 1. examples/assignments.nacl
+for i in "${examples[@]}"
+do
+	cat ../examples/$i.nacl | ../NaCl.py $CPP_OUTPUT
+	DIFFERENCE=`diff $CPP_OUTPUT ../$GOLDENFILES/$i.cpp`
+	echo "$DIFFERENCE" > diff_output.txt
 
-cat ../examples/assignments.nacl | ../NaCl.py nacl_content.cpp
-DIFFERENCE=`diff nacl_content.cpp ../goldenfiles/assignments.cpp`
-
-echo "$DIFFERENCE" > diff_output.txt
-
-if [ -z "$DIFFERENCE" ]
-then
-	echo "Assignments: Success"
-else
-	echo "Assignments: Failed"
-fi
-
-# 2. examples/cidr.nacl
-
-cat ../examples/cidr.nacl | ../NaCl.py nacl_content.cpp
-DIFFERENCE=`diff nacl_content.cpp ../goldenfiles/cidr.cpp`
-
-echo "$DIFFERENCE" > diff_output.txt
-
-if [ -z "$DIFFERENCE" ]
-then
-	echo "Cidr: Success"
-else
-	echo "Cidr: Failed"
-fi
-
-# 3. examples/config_options.nacl
-
-cat ../examples/config_options.nacl | ../NaCl.py nacl_content.cpp
-DIFFERENCE=`diff nacl_content.cpp ../goldenfiles/config_options.cpp`
-
-echo "$DIFFERENCE" > diff_output.txt
-
-if [ -z "$DIFFERENCE" ]
-then
-	echo "Config options: Success"
-else
-	echo "Config options: Failed"
-fi
-
-# 4. examples/conntrack.nacl
-
-cat ../examples/conntrack.nacl | ../NaCl.py nacl_content.cpp
-DIFFERENCE=`diff nacl_content.cpp ../goldenfiles/conntrack.cpp`
-
-echo "$DIFFERENCE" > diff_output.txt
-
-if [ -z "$DIFFERENCE" ]
-then
-	echo "Conntrack: Success"
-else
-	echo "Conntrack: Failed"
-fi
-
-# 5. examples/gateway_with_send_time_exceeded.nacl
-
-cat ../examples/gateway_with_send_time_exceeded.nacl | ../NaCl.py nacl_content.cpp
-DIFFERENCE=`diff nacl_content.cpp ../goldenfiles/gateway_with_send_time_exceeded.cpp`
-
-echo "$DIFFERENCE" > diff_output.txt
-
-if [ -z "$DIFFERENCE" ]
-then
-	echo "Gateway with send time exceeded: Success"
-else
-	echo "Gateway with send time exceeded: Failed"
-fi
-
-# 6. examples/iface.nacl
-
-cat ../examples/iface.nacl | ../NaCl.py nacl_content.cpp
-DIFFERENCE=`diff nacl_content.cpp ../goldenfiles/iface.cpp`
-
-echo "$DIFFERENCE" > diff_output.txt
-
-if [ -z "$DIFFERENCE" ]
-then
-	echo "Iface: Success"
-else
-	echo "Iface: Failed"
-fi
-
-# 7. examples/log.nacl
-
-cat ../examples/log.nacl | ../NaCl.py nacl_content.cpp
-DIFFERENCE=`diff nacl_content.cpp ../goldenfiles/log.cpp`
-
-echo "$DIFFERENCE" > diff_output.txt
-
-if [ -z "$DIFFERENCE" ]
-then
-	echo "Log: Success"
-else
-	echo "Log: Failed"
-fi
-
-# 8. examples/nacl.nacl
-
-cat ../examples/nacl.nacl | ../NaCl.py nacl_content.cpp
-DIFFERENCE=`diff nacl_content.cpp ../goldenfiles/nacl.cpp`
-
-echo "$DIFFERENCE" > diff_output.txt
-
-if [ -z "$DIFFERENCE" ]
-then
-	echo "Nacl: Success"
-else
-	echo "Nacl: Failed"
-fi
-
-# 9. examples/nacl_one_liner.nacl
-
-cat ../examples/nacl_one_liner.nacl | ../NaCl.py nacl_content.cpp
-DIFFERENCE=`diff nacl_content.cpp ../goldenfiles/nacl_one_liner.cpp`
-
-echo "$DIFFERENCE" > diff_output.txt
-
-if [ -z "$DIFFERENCE" ]
-then
-	echo "Nacl one liner: Success"
-else
-	echo "Nacl one liner: Failed"
-fi
-
-# 10. examples/nat_and_gateway.nacl
-
-cat ../examples/nat_and_gateway.nacl | ../NaCl.py nacl_content.cpp
-DIFFERENCE=`diff nacl_content.cpp ../goldenfiles/nat_and_gateway.cpp`
-
-echo "$DIFFERENCE" > diff_output.txt
-
-if [ -z "$DIFFERENCE" ]
-then
-	echo "Nat and gateway: Success"
-else
-	echo "Nat and gateway: Failed"
-fi
-
-# 11. examples/vlan.nacl
-
-cat ../examples/vlan.nacl | ../NaCl.py nacl_content.cpp
-DIFFERENCE=`diff nacl_content.cpp ../goldenfiles/vlan.cpp`
-
-echo "$DIFFERENCE" > diff_output.txt
-
-if [ -z "$DIFFERENCE" ]
-then
-	echo "Vlan: Success"
-else
-	echo "Vlan: Failed"
-fi
+	if [ -z "$DIFFERENCE" ]
+	then
+		echo "$i: Success"
+	else
+		echo "$i: Failed"
+	fi
+done
