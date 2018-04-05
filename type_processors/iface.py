4d30c882c11bb8d9e243c98f1a26962824edfaeb
Annika Hammervoll <annika_limfe@hotmail.com>
2018-04-05 15:06:32 +0200

    type_processors/iface.py: Defining template key constants (and no longer importing these from NaCl.py)

--- type_processors/iface.py
+++ type_processors/iface.py
@@ -1,13 +1,13 @@
 from __future__ import absolute_import
 # To avoid: <...>/NaCl/type_processors/iface.py:1: RuntimeWarning: Parent module '<...>/NaCl/type_processors' not found while handling absolute import
 
+# TODO
 from NaCl import exit_NaCl, NaCl_exception, Typed, get_line_and_column, \
 	BASE_TYPE_TYPED_INIT, BASE_TYPE_FUNCTION, \
 	DOT, TRUE, FALSE, TEMPLATE_KEY_IFACE, \
 	TYPE_NAT, TEMPLATE_KEY_FUNCTION_NAME, TEMPLATE_KEY_COMMA, \
 	TEMPLATE_KEY_NAME, TEMPLATE_KEY_CHAIN, TEMPLATE_KEY_FUNCTION_NAMES, \
-	TEMPLATE_KEY_INDEX, TEMPLATE_KEY_NETMASK, TEMPLATE_KEY_GATEWAY, TEMPLATE_KEY_ADDRESS, \
-	TEMPLATE_KEY_IFACE_INDEX, TEMPLATE_KEY_TITLE, TEMPLATE_KEY_DNS, \
+	TEMPLATE_KEY_IFACE_INDEX, TEMPLATE_KEY_TITLE, \
 	TEMPLATE_KEY_HAS_NATS, \
 	elements
 '''
@@ -111,6 +111,12 @@ TEMPLATE_KEY_HAS_MASQUERADES 			= "has_masquerades"
 TEMPLATE_KEY_CONFIG_IS_DHCP 			= "config_is_dhcp"
 TEMPLATE_KEY_CONFIG_IS_DHCP_FALLBACK 	= "config_is_dhcp_fallback"
 TEMPLATE_KEY_CONFIG_IS_STATIC 			= "config_is_static"
+
+TEMPLATE_KEY_INDEX 						= "index"
+TEMPLATE_KEY_ADDRESS 					= "address"
+TEMPLATE_KEY_NETMASK 					= "netmask"
+TEMPLATE_KEY_GATEWAY 					= "gateway"
+TEMPLATE_KEY_DNS 						= "dns"
 
 # -------------------- CLASSES --------------------
 
