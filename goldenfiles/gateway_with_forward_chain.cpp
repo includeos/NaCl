0c518f2cda5a93db033317419221447abcc60e3c
Annika Hammervoll <annika_limfe@hotmail.com>
2018-05-31 17:39:10 +0200

    Updating copyright to include 2018

--- goldenfiles/gateway_with_forward_chain.cpp
+++ goldenfiles/gateway_with_forward_chain.cpp
@@ -1,6 +1,6 @@
 // This file is a part of the IncludeOS unikernel - www.includeos.org
 //
-// Copyright 2017 IncludeOS AS, Oslo, Norway
+// Copyright 2017-2018 IncludeOS AS, Oslo, Norway
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
